9bc0ffa0a3e84269aa9c615e5a1079aba4b20d18
veerendra thogiti <thogitiveerendra5@gmail.com>
2025-07-05 09:59:26 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Veerendra21@',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'frigga_kb',
   port: process.env.DB_PORT || 3306,
   charset: 'utf8mb4',
